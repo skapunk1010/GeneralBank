2523fd1828552762cacabc54fd4ce33f425e5e1c
Erick Martínez <s_e_b_a_s_1010@hotmail.com>
2014-10-23 11:57:30 -0500

    Clase Movimiento.cs

---
+++ GeneralBank/Clases/Movimiento.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralBank.Clases
+{
+    class Movimiento
+    {
+        public Movimiento(int id, Enum tipo, String d, String f, Empleado e) {
+            this.idMovimiento = id;
+            this.tipo = tipo;
+            this.descripcion = d;
+            this.fecha = f;
+            this.empleado = e;
+        }
+
+        private int idMovimiento;
+        public int IdMovimiento
+        {
+            get { return idMovimiento; }
+            set { idMovimiento = value; }
+        }
+
+        private Enum tipo;
+        public Enum Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+
+        private String descripcion;
+        public String Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+
+        private String fecha;
+        public String Fecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+        private Empleado empleado;
+        public Empleado Empleado
+        {
+            get { return empleado; }
+            set { empleado = value; }
+        }
+    }
+}
