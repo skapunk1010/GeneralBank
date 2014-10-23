d02ebaa8a3b4b5262cc1b7cacfe818525f03a343
Erick Martínez <s_e_b_a_s_1010@hotmail.com>
2014-10-23 12:21:28 -0500

    Clase Sucursal.cs

---
+++ GeneralBank/Clases/Sucursal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralBank.Clases
+{
+    class Sucursal
+    {
+        public Sucursal() { 
+        }
+
+        private Empleado[] empleados;
+        public Empleado[] Empleados
+        {
+            get { return empleados; }
+            set { empleados = value; }
+        }
+
+        private Cliente[] clientes;
+        public Cliente[] Clientes
+        {
+            get { return clientes; }
+            set { clientes = value; }
+        }
+
+        private Domicilio domicilio;
+        public Domicilio Domicilio
+        {
+            get { return domicilio; }
+            set { domicilio = value; }
+        }
+
+        private Telefono telefono;
+        public Telefono Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+    }
+}
