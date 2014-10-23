25db80d74c27476827797179e3276a045ecf6a70
Erick Martínez <s_e_b_a_s_1010@hotmail.com>
2014-10-23 12:14:47 -0500

    Clase Telefono.cs

---
+++ GeneralBank/Clases/Telefono.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralBank.Clases
+{
+    class Telefono
+    {
+        public Telefono(String lada, String tel, String tipo) {
+            this.lada = lada;
+            this.telefono = tel;
+            this.tipo = tipo;
+        }
+
+        private String lada;
+        public String Lada
+        {
+            get { return lada; }
+            set { lada = value; }
+        }
+
+        private String telefono;
+        public String Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        private String tipo;
+        public String Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+    }
+}
