362e3621048d23776fb86b87e15e3729cc86ce6f
Erick Martínez <s_e_b_a_s_1010@hotmail.com>
2014-10-23 11:58:37 -0500

    Clase Transferencia.cs

---
+++ GeneralBank/Clases/Transferencia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralBank.Clases
+{
+    class Transferencia : Movimiento
+    {
+        public Transferencia(int id, Enum tipo, String d, String f, Empleado e,float m, String destino): base(id,tipo,d,f,e) {
+            this.monto = m;
+            this.numCuentaDestino = destino;
+        }
+
+        private float monto;
+        public float Monto
+        {
+            get { return monto; }
+            set { monto = value; }
+        }
+
+        private String numCuentaDestino;
+        public String NumCuentaDestino
+        {
+            get { return numCuentaDestino; }
+            set { numCuentaDestino = value; }
+        }
+    }
+}
