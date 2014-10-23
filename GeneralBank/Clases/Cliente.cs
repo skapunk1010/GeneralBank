4711e7d3364b1532d80eff0f85cedb06a295f2e8
Erick Martínez <s_e_b_a_s_1010@hotmail.com>
2014-10-23 02:45:56 -0500

    Clase Cliente.cs

---
+++ GeneralBank/Clases/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralBank.Clases
+{
+    class Cliente : Persona
+    {
+        private String[] cuentasAsociada;
+        public String[] CuentasAsociadas
+        {
+            get { return cuentasAsociada; }
+            set { cuentasAsociada = value}
+        }
+    }
+}
