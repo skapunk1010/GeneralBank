2e1979c78207ad2f6dde7a0fb8d3f70fe4b552cc
Erick Martínez <s_e_b_a_s_1010@hotmail.com>
2014-10-24 00:45:49 -0500

    Formulario AgregarCuenta

---
+++ GeneralBank/AgregarCuenta.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace GeneralBank
+{
+    partial class AgregarCuenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumeroCuenta = new System.Windows.Forms.Label();
+            this.lblTipoCuenta = new System.Windows.Forms.Label();
+            this.lblSucursal = new System.Windows.Forms.Label();
+            this.lblEmpleado = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dropTipoCuenta = new System.Windows.Forms.ComboBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumeroCuenta
+            // 
+            this.lblNumeroCuenta.AutoSize = true;
+            this.lblNumeroCuenta.Location = new System.Drawing.Point(26, 40);
+            this.lblNumeroCuenta.Name = "lblNumeroCuenta";
+            this.lblNumeroCuenta.Size = new System.Drawing.Size(72, 13);
+            this.lblNumeroCuenta.TabIndex = 0;
+            this.lblNumeroCuenta.Text = "Num. Cuenta:";
+            // 
+            // lblTipoCuenta
+            // 
+            this.lblTipoCuenta.AutoSize = true;
+            this.lblTipoCuenta.Location = new System.Drawing.Point(26, 85);
+            this.lblTipoCuenta.Name = "lblTipoCuenta";
+            this.lblTipoCuenta.Size = new System.Drawing.Size(68, 13);
+            this.lblTipoCuenta.TabIndex = 1;
+            this.lblTipoCuenta.Text = "Tipo Cuenta:";
+            // 
+            // lblSucursal
+            // 
+            this.lblSucursal.AutoSize = true;
+            this.lblSucursal.Location = new System.Drawing.Point(277, 40);
+            this.lblSucursal.Name = "lblSucursal";
+            this.lblSucursal.Size = new System.Drawing.Size(51, 13);
+            this.lblSucursal.TabIndex = 2;
+            this.lblSucursal.Text = "Sucursal:";
+            // 
+            // lblEmpleado
+            // 
+            this.lblEmpleado.AutoSize = true;
+            this.lblEmpleado.Location = new System.Drawing.Point(277, 85);
+            this.lblEmpleado.Name = "lblEmpleado";
+            this.lblEmpleado.Size = new System.Drawing.Size(57, 13);
+            this.lblEmpleado.TabIndex = 3;
+            this.lblEmpleado.Text = "Empleado:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(334, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(334, 82);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(145, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // dropTipoCuenta
+            // 
+            this.dropTipoCuenta.FormattingEnabled = true;
+            this.dropTipoCuenta.Location = new System.Drawing.Point(104, 82);
+            this.dropTipoCuenta.Name = "dropTipoCuenta";
+            this.dropTipoCuenta.Size = new System.Drawing.Size(145, 21);
+            this.dropTipoCuenta.TabIndex = 7;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(208, 146);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 8;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(307, 146);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 9;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(404, 146);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // AgregarCuenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 203);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.dropTipoCuenta);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblEmpleado);
+            this.Controls.Add(this.lblSucursal);
+            this.Controls.Add(this.lblTipoCuenta);
+            this.Controls.Add(this.lblNumeroCuenta);
+            this.Name = "AgregarCuenta";
+            this.Text = "Agregar Cuenta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumeroCuenta;
+        private System.Windows.Forms.Label lblTipoCuenta;
+        private System.Windows.Forms.Label lblSucursal;
+        private System.Windows.Forms.Label lblEmpleado;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox dropTipoCuenta;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
