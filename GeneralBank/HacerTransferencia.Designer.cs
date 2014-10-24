fe0e759ed4552fa0345033d84dbba547e5d7f7f4
Erick Martínez <s_e_b_a_s_1010@hotmail.com>
2014-10-24 01:48:04 -0500

    Formulario HacerTransferencias

---
+++ GeneralBank/HacerTransferencia.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace GeneralBank
+{
+    partial class HacerTransferencia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCuentaOrigen = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCuentaOrigen = new System.Windows.Forms.TextBox();
+            this.txtCuentaDestino = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCuentaOrigen
+            // 
+            this.lblCuentaOrigen.AutoSize = true;
+            this.lblCuentaOrigen.Location = new System.Drawing.Point(61, 45);
+            this.lblCuentaOrigen.Name = "lblCuentaOrigen";
+            this.lblCuentaOrigen.Size = new System.Drawing.Size(130, 13);
+            this.lblCuentaOrigen.TabIndex = 0;
+            this.lblCuentaOrigen.Text = "Número de cuenta origen:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número de cuenta destino:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cantidad a transferir:";
+            // 
+            // txtCuentaOrigen
+            // 
+            this.txtCuentaOrigen.Location = new System.Drawing.Point(230, 42);
+            this.txtCuentaOrigen.Name = "txtCuentaOrigen";
+            this.txtCuentaOrigen.Size = new System.Drawing.Size(184, 20);
+            this.txtCuentaOrigen.TabIndex = 3;
+            // 
+            // txtCuentaDestino
+            // 
+            this.txtCuentaDestino.Location = new System.Drawing.Point(230, 84);
+            this.txtCuentaDestino.Name = "txtCuentaDestino";
+            this.txtCuentaDestino.Size = new System.Drawing.Size(184, 20);
+            this.txtCuentaDestino.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(196, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(199, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(454, 82);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar.TabIndex = 6;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(320, 115);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 7;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(207, 115);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnAceptar);
+            this.groupBox1.Location = new System.Drawing.Point(64, 144);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(465, 224);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // HacerTransferencia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 412);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.txtCuentaDestino);
+            this.Controls.Add(this.txtCuentaOrigen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCuentaOrigen);
+            this.MaximumSize = new System.Drawing.Size(610, 450);
+            this.MinimumSize = new System.Drawing.Size(610, 450);
+            this.Name = "HacerTransferencia";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hacer Transferencia";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCuentaOrigen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCuentaOrigen;
+        private System.Windows.Forms.TextBox txtCuentaDestino;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
