0886dcef7656789d04a62b7c4e353cde9f04e46e
Erick Martínez <s_e_b_a_s_1010@hotmail.com>
2014-10-24 01:48:45 -0500

    Cambio de nombre a control GroupBox

---
+++ GeneralBank/HacerDeposito.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace GeneralBank
+{
+    partial class HacerDeposito
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumCuenta = new System.Windows.Forms.Label();
+            this.lblCantDepositar = new System.Windows.Forms.Label();
+            this.txtNumCuenta = new System.Windows.Forms.TextBox();
+            this.txtCantDepositar = new System.Windows.Forms.TextBox();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNumCuenta
+            // 
+            this.lblNumCuenta.AutoSize = true;
+            this.lblNumCuenta.Location = new System.Drawing.Point(44, 53);
+            this.lblNumCuenta.Name = "lblNumCuenta";
+            this.lblNumCuenta.Size = new System.Drawing.Size(98, 13);
+            this.lblNumCuenta.TabIndex = 0;
+            this.lblNumCuenta.Text = "Número de cuenta:";
+            // 
+            // lblCantDepositar
+            // 
+            this.lblCantDepositar.AutoSize = true;
+            this.lblCantDepositar.Location = new System.Drawing.Point(51, 49);
+            this.lblCantDepositar.Name = "lblCantDepositar";
+            this.lblCantDepositar.Size = new System.Drawing.Size(107, 13);
+            this.lblCantDepositar.TabIndex = 1;
+            this.lblCantDepositar.Text = "Cantidad a depositar:";
+            // 
+            // txtNumCuenta
+            // 
+            this.txtNumCuenta.Location = new System.Drawing.Point(159, 50);
+            this.txtNumCuenta.Name = "txtNumCuenta";
+            this.txtNumCuenta.Size = new System.Drawing.Size(225, 20);
+            this.txtNumCuenta.TabIndex = 2;
+            // 
+            // txtCantDepositar
+            // 
+            this.txtCantDepositar.Location = new System.Drawing.Point(200, 46);
+            this.txtCantDepositar.Name = "txtCantDepositar";
+            this.txtCantDepositar.Size = new System.Drawing.Size(204, 20);
+            this.txtCantDepositar.TabIndex = 3;
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(419, 48);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(75, 23);
+            this.btnValidar.TabIndex = 4;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(200, 127);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 5;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(329, 127);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 5;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAceptar);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.txtCantDepositar);
+            this.groupBox1.Controls.Add(this.lblCantDepositar);
+            this.groupBox1.Location = new System.Drawing.Point(37, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(457, 207);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // HacerDeposito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 357);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.txtNumCuenta);
+            this.Controls.Add(this.lblNumCuenta);
+            this.MaximumSize = new System.Drawing.Size(550, 395);
+            this.MinimumSize = new System.Drawing.Size(550, 395);
+            this.Name = "HacerDeposito";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hacer Depósito";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumCuenta;
+        private System.Windows.Forms.Label lblCantDepositar;
+        private System.Windows.Forms.TextBox txtNumCuenta;
+        private System.Windows.Forms.TextBox txtCantDepositar;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
