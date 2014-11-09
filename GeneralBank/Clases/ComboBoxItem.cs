cbd98e082d09f6d2f70ee79247f0f1588884da07
Erick Martínez <s_e_b_a_s_1010@hotmail.com>
2014-11-09 15:00:03 -0600

    Olvidé ésta clase necesaria para el último form

---
+++ GeneralBank/Clases/ComboBoxItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeneralBank.Clases
+{
+    class ComboBoxItem
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public ComboBoxItem(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
