976caebccdb000f403a023644e7b23dcc5ab1bbc
Paz <irenapaz@yahoo.com>
2014-11-10 13:52:00 -0600

    agregando clase cuenta, cambios a tipos de dato

--- GeneralBank/Clases/Cliente.cs
+++ GeneralBank/Clases/Cliente.cs
@@ -7,13 +7,13 @@ namespace GeneralBank.Clases
 {
     class Cliente : Persona
     {
-        public Cliente(String n, String ap, String am, String fechaNac,String[] cuentas):base(n,ap,am,fechaNac) 
+        public Cliente(String n, String ap, String am, String fechaNac, Cuenta[] cuentas):base(n,ap,am,fechaNac) 
         {
             this.cuentasAsociada = cuentas;
         }
 
-        private String[] cuentasAsociada;
-        public String[] CuentasAsociadas
+        private Cuenta[] cuentasAsociada;
+        public Cuenta[] CuentasAsociadas
         {
             get { return cuentasAsociada; }
             set { cuentasAsociada = value; }
